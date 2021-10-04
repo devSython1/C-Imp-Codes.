aa4e93193e7232666a583d3f12ce629208c0d35c
devSython1 <73010511+devSython1@users.noreply.github.com>
2021-10-04 07:53:06 +0530

    Update Armstrong number.c

--- Armstrong number.c
+++ Armstrong number.c
@@ -5,36 +5,28 @@
 
 int main()
 {
-	int user_input,result=0,digit,power=0;
+	int user_input,digit=0,power=0;
     printf("Enter a Number: ");
     scanf("%d",&user_input);
     
-	int num1,num2;
-	num1 = user_input;
-    num2 = user_input;
+    int num1 = user_input;
     
     while(num1)
     {
-    	power = power + 1;
-    	num1 = num1/10;
-	}
-    
-	while(user_input)
-    {
-        digit = user_input%10;
-        result += pow(digit,power);
-        user_input = user_input/10;
+	    digit = num1%10;
+	    power += pow(digit,3);
+	    num1 /= 10;
     }
     
     printf("Sum of (raise to %d) power of each digit of the Given Number = %d",power,result);
     
-	if(result == num2)
-    {
-    	printf("\n\n%d = %d\nTherefor Entered Number is an Armstrong",num2,result);
+	if(power == user_input)
+        {
+    		printf("\n\n%d = %d\nTherefor Entered Number is an Armstrong",num2,result);
 	}
 	else
 	{
-	    printf("\n\n%d != %d\nTherefor Entered Number is not an Armstrong",num2,result);
+	    	printf("\n\n%d != %d\nTherefor Entered Number is not an Armstrong",num2,result);
 	}
     
 	return 0;
