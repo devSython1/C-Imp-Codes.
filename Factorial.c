e93207a487429a979922bbf90a73eacdf2b8175c
devSython1 <73010511+devSython1@users.noreply.github.com>
2021-10-04 07:45:52 +0530

    Update Factorial.c

--- Factorial.c
+++ Factorial.c
@@ -18,12 +18,26 @@ unsigned long factorial(int temp)
    
 int main()  
 {  
-	int user_input;  
+	int user_input, fact = 1;  
 	unsigned long result;  
 	printf("Enter a number: ");  
 	scanf("%d", &user_input);   
+	
+	for(int i = 1 ; i <= user_input ; i++) 	  // factorial using for loop
+	{
+		if(user_input == 0)
+		{
+			printf("1");
+		}
+		else
+		{
+			fact = fact * i;
+		}
+	}
+	
+	printf("\nFactorial of %d is %lu", user_input, fact);
    
-	result = factorial(user_input);  
+	result = factorial(user_input);  // Recursive approach
 	if(user_input == 0)
 	{
 		printf("\nFactorial of '0' is 1");
